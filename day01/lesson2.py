e72e7aacea0e3845b522001becd85097b7c619df
snovt <kamazitsutsqo@gmail.com>
2024-05-12 22:34:45 +0400

    first commit

---
+++ day01/lesson2.py
@@ -0,0 +1,31 @@
+name ="ramazi"
+
+nn = "me"
+
+nm = "var"
+
+uu = "wlis"
+
+surname = "tsutskiridze"
+
+wat = "vtamashob"
+
+game = "fnafs"
+
+hh = "wlis"
+
+age = 13
+height = 158.5
+
+has_a_headphonedent = True
+plays_fnaf = True
+is_virgin = False
+
+isVirgin = False
+ 
+print(name + " " + surname)
+
+#print(name + age) = str + int 
+
+
+print(nn + " " + nm + " " + name + " " + surname + " " + nm + " " + str(age) + " " + hh + " " + wat + " " + game)
