769cd8621ca9277a72295d8cc472768ff380afc8
Snovt <kamazitsutsqo@gmail.com>
2024-05-14 17:25:22 +0400

    Add files via upload

---
+++ day02/lesson3.py
@@ -0,0 +1,9 @@
+person1 = "nika taktakashvili"
+person2 ="deme biganashvili"
+person3 ="gochiko aspanidze"
+person4 = "giorgi kintsurashvili"
+person5 = "george gvirilashvili"
+person6 = "andria jangulia"
+
+
+print(person1 + " " + person2 + " " + person3 + " " + person4 + " " + person5 + " " + person6)
