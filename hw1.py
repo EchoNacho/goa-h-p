a8171900cd4a27111231a4a7333eac33f6d63024
EchoNacho <kamazitsutsqo@gmail.com>
2024-08-14 13:59:35 +0400

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,18 @@
+# Taking input from the user
+num1 = float(input("3: "))
+num2 = float(input("2: "))
+num3 = float(input("5: "))
+num4 = float(input("4: "))
+# Performing arithmetic operations 
+sum_result = num1 + num2 + num3 + num4 
+difference_result = num1 num2 num3 num4
+product_result = num1 * num2 * num3 * num4
+integer_division_result = num1 // num2 // num3 // num4 
+power_result = num1 ** num2 ** num3 ** num4
+
+# Displaying results
+print(f"Sum: {sum_result}")
+print(f"Difference: {difference_result}")
+print(f"Product: {product_result}")
+print (f" Integer Division: (integer_division_result}") 
+print(f"Power: {power_result}")
