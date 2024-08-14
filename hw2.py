c03ba3502907ba19d718a0912679472be622997e
EchoNacho <kamazitsutsqo@gmail.com>
2024-08-14 14:15:00 +0400

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,17 @@
+# Input the ages of your family members
+mother_age = int(input("37: "))
+father_age = int(input("42: "))
+sister_age = int(input("10: "))
+brother_age = int(input("3: "))
+
+# Add 20 years to each member's current age age_in_20_years_member1 = member1_age + 20
+age_in_20_years_mother = mother_age + 20
+age_in_20_years_father = father_age + 20
+age_in_20_years_sister = sister_age + 20
+age_in_20_years_brother = brother_age + 20
+
+# Print the results
+print(f"In 20 years, the mother will be {age_in_20_years_mother} years old.")
+print(f"In 20 years, the father will be {age_in_20_years_father} years old.") 
+print(f"In 20 years, the sister will be {age_in_20_years_sister} years old.")
+print(f"In 20 years, the brother will be {age_in_20_years_brother}years old.")
