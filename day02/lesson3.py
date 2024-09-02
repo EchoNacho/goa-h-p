c6c2d1d4b62db59a77d0e24877da7075032b0a20
EchoNacho <kamazitsutsqo@gmail.com>
2024-09-02 16:29:18 +0400

    Update lesson3.py

--- day02/lesson3.py
+++ day02/lesson3.py
@@ -2,8 +2,8 @@ person1 = "nika taktakashvili"
 person2 ="deme biganashvili"
 person3 ="gochiko aspanidze"
 person4 = "giorgi kintsurashvili"
-person5 = "george gvirilashvili"
-person6 = "andria jangulia"
+person5 = "saba Chachanidze"
+person6 = "luka wulaia"
 
 
 print(person1 + " " + person2 + " " + person3 + " " + person4 + " " + person5 + " " + person6)
